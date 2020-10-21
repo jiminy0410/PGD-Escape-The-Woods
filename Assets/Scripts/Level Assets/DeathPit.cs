760983c8d424185de910ad34384e9338644182bb
DenVosReinaert <adriaan.fazant@live.nl>
2020-10-21 10:37:37 +0200

    Scripts Imported

---
+++ Assets/Scripts/Level Assets/DeathPit.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathPit : MonoBehaviour
+{
+    public GameObject Flash;
+    public GameObject player = null;
+    public Transform respawnPoint;
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        Flash = GameObject.Find("PlayerVision");
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        player.transform.position = respawnPoint.position;
+        Flash.GetComponent<FlashMechanic>().StartCoroutine("Flash");
+    }
+}
---
+++ Assets/Scripts/Misc/Player/FlashMechanic.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class FlashMechanic : MonoBehaviour
+{
+    //public Light2D playerVision;
+    public static float maxFlashCharges = 3;
+    public static float flashCharges = maxFlashCharges;
+    public static float lightDecay = 3f;
+    public static float defaultOuterRadius = 2;
+
+    private static float enlargedOuterRadius = 10;
+
+    private AudioSource flashSound;
+
+
+    void Start()
+    {
+        //playerVision = GetComponent<Light2D>();
+        flashSound = this.GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            if (flashCharges >= 1)
+            {
+                StartCoroutine(Flash());
+                flashCharges--;
+            }
+        }
+
+
+        /*
+        if (playerVision.pointLightOuterRadius > defaultOuterRadius)
+        {
+            playerVision.pointLightOuterRadius -= lightDecay * Time.deltaTime;
+            Debug.Log(lightDecay);
+        }
+        */
+
+    }
+
+    public IEnumerator Flash()
+    {
+        //playerVision.pointLightOuterRadius = enlargedOuterRadius;
+        flashSound.Play();
+        yield return null;
+    }
+}
