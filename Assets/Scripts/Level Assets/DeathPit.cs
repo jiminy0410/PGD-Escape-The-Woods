4785291141e8a130bd834400263124086241ce0a
yves <yvesprinz59@gmail.com>
2020-10-26 14:31:45 +0100

    deathpit fixes
    it now do

--- Assets/Scripts/Level Assets/DeathPit.cs
+++ Assets/Scripts/Level Assets/DeathPit.cs
@@ -9,13 +9,13 @@ public class DeathPit : MonoBehaviour
     public Transform respawnPoint;
     void Start()
     {
-        player = GameObject.Find("Player");
+        player = GameObject.Find("player");
         Flash = GameObject.Find("PlayerVision");
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         player.transform.position = respawnPoint.position;
-        Flash.GetComponent<FlashMechanic>().StartCoroutine("Flash");
+        //Flash.GetComponent<FlashMechanic>().StartCoroutine("Flash");
     }
 }
