4d41d757316418e2bb9e5acb2da41216e991b47d
ST34M <niels.van.leusden@hva.nl>
2020-11-03 16:00:45 +0100

    Regenerating Flashes is now working
    + Flashes now slowly recharge after 5 seconds (can be changed)
    + Current max of one charge (can be changed)
    + Use of a bar in the top right to display the charge slowly recharging, slowly charges up to full

--- Assets/Scripts/Misc/Player/FlashMechanic.cs
+++ Assets/Scripts/Misc/Player/FlashMechanic.cs
@@ -7,8 +7,9 @@ using UnityEngine.Experimental.Rendering.Universal;
 public class FlashMechanic : MonoBehaviour
 {
     public Light2D playerVision;
-    public float maxFlashCharges = 3;
+    public float maxFlashCharges = 1;
     public float flashCharges;
+    public float flashRechargeRate = 0.2f;
     public static float lightDecay = 3f;
     public static float defaultOuterRadius = 2;
 
@@ -16,34 +17,49 @@ public class FlashMechanic : MonoBehaviour
 
     private AudioSource flashSound;
 
+    public FlashBar flashBar;
+
 
     void Start()
     {
         flashCharges = maxFlashCharges;
         playerVision = GetComponent<Light2D>();
         flashSound = this.GetComponent<AudioSource>();
+        flashBar.SetMaxCharges(maxFlashCharges);
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Q))
         {
-            if (flashCharges >= 1)
+            if (flashCharges == maxFlashCharges)
             {
                 StartCoroutine(Flash());
                 flashCharges--;
             }
         }
 
-        Debug.Log(playerVision.pointLightOuterRadius);
-        
+        if (flashCharges < maxFlashCharges)
+        {
+            flashCharges += flashRechargeRate * Time.deltaTime;
+        }
+
         if (playerVision.pointLightOuterRadius > defaultOuterRadius)
         {
             playerVision.pointLightOuterRadius -= lightDecay * Time.deltaTime;
-            Debug.Log(lightDecay);
         }
-        
 
+        if (flashCharges > 1)
+        {
+            flashCharges = 1;
+        }
+
+        if (playerVision.pointLightOuterRadius < 2)
+        {
+            playerVision.pointLightOuterRadius = 2;
+        }
+
+        flashBar.SetCharges(flashCharges);
     }
 
     public IEnumerator Flash()
