561ebc61057aa5ef8029bca19486a5aa37a10f1f
ST34M <niels.van.leusden@hva.nl>
2020-11-06 13:54:25 +0100

    Added Double Jump Item
    + Made the script to update player's maximum jumps
    + Added a prefab with a picture of boots, with the script attached
    + Added picture of boots

---
+++ Assets/DoubleJump.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoubleJump : MonoBehaviour
+{
+    GameObject player;
+    CharacterController2D doubleJumpPlayer;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        doubleJumpPlayer = player.GetComponent<CharacterController2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            doubleJumpPlayer.Max_Jumps = 1;
+            Destroy(gameObject);
+        }
+    }
+}
