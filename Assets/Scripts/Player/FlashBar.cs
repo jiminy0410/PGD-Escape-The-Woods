305c8f9f71b4d227a18e941eb74b23ffb13d7d74
yves <yvesprinz59@gmail.com>
2020-11-16 16:58:14 +0100

    dynamic charge bar colors
    wheeeeeeeeeeeeeeeeeeeeeeeeeeeeeee

--- Assets/Scripts/Player/FlashBar.cs
+++ Assets/Scripts/Player/FlashBar.cs
@@ -7,6 +7,25 @@ public class FlashBar : MonoBehaviour
 {
 
     public Slider slider;
+    public Image phill;
+
+    public FlashMechanic theFlash;
+
+    public Color emptyColor, halfColor, fullColor;
+
+    private void Update()
+    {
+        if (theFlash.currentFlashRechargeRate > theFlash.ChargeMax/2)
+        {
+            phill.color = Color.Lerp(halfColor, fullColor, (theFlash.currentFlashRechargeRate- theFlash.ChargeMax / 2) / (theFlash.ChargeMax - theFlash.ChargeMax / 2));
+        }
+        else
+        {
+            phill.color = Color.Lerp(emptyColor, halfColor, theFlash.currentFlashRechargeRate / (theFlash.ChargeMax/2));
+        }
+        
+        phill.color = new Color(phill.color.r, phill.color.g, phill.color.b, 1);
+    }
 
     public void SetMaxCharges(float charges)
     {
