563b01898e1c05eea0dd7d868760c01434c06e86
Adriaan Pronk <adriaan.fazant@live.nl>
2020-12-01 21:49:14 +0100

    SecretWalls Complete
    +Upon using the flash the wall will reach an opacity of 0
    +When the player is overlapping with the wall, the wall will become semi-transparent

---
+++ Assets/Scripts/Misc/SecretWallComponents.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class SecretWallComponents : MonoBehaviour
+{
+
+    [HideInInspector]
+    public float tileAlpha;
+
+    public float opacityRecovery;
+    public float playerCollisionOpacity;
+
+    [HideInInspector]
+    public Tilemap tileRenderer;
+
+    [HideInInspector]
+    public bool collidingWithPlayer;
+
+
+
+
+}
---
+++ Assets/Scripts/Misc/SecretWallSystems.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class SecretWallSystems : SecretWallComponents
+{
+ 
+    void Start()
+    {
+        tileRenderer = this.GetComponent<Tilemap>();
+    }
+
+
+    void Update()
+    {
+        if (!collidingWithPlayer)
+        {
+            tileAlpha += 0.0255f * opacityRecovery;
+        }
+        else
+        {
+            tileAlpha = 0.0255f * playerCollisionOpacity;
+        }
+     
+
+
+
+
+
+        tileRenderer.color = new Color(tileRenderer.color.r, tileRenderer.color.g, tileRenderer.color.b, tileAlpha);
+        
+    }
+
+    public void MakeTransparent()
+    {
+        tileAlpha = 0f;
+    }
+
+
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player") && collision == GameObject.Find("Player").GetComponent<BoxCollider2D>())
+        {
+            collidingWithPlayer = true;
+            Debug.Log("lol");
+        }
+        
+
+
+        
+    }
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+            collidingWithPlayer = false;
+    }
+}
