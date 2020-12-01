f63fe9b03cd35f1818715feff14bdb2290562cc1
Adriaan Pronk <adriaan.fazant@live.nl>
2020-12-01 23:46:46 +0100

    Setting the stage
    +Made some prefabs just to make level setup more convenient
    +Campfire has been added to give the wells more functionality

---
+++ Assets/Scripts/Level Assets/CampfireComponents.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CampfireComponents : MonoBehaviour
+{
+
+
+
+    public List<GameObject> objectsToActivate;
+    public List<BoxCollider2D> wellTriggers;
+
+    [HideInInspector]
+    public bool acceptablePlayerPos;
+
+}
---
+++ Assets/Scripts/Level Assets/CampfireSystems.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CampfireSystems : CampfireComponents
+{
+
+    void Update()
+    {
+        if (acceptablePlayerPos && Input.GetAxisRaw("Vertical") > 0)
+        {
+            foreach (GameObject oTA in objectsToActivate)
+            {
+                oTA.SetActive(true);
+            }
+
+            foreach (BoxCollider2D wellTrigger in wellTriggers)
+            {
+                wellTrigger.enabled = true;
+            }
+        }
+
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        acceptablePlayerPos = true;
+    }
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        acceptablePlayerPos = false;
+    }
+}
