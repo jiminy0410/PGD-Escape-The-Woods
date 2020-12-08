4b32404b90dfe35043b27eb4c432fdd777dae4d2
Adriaan Pronk <adriaan.fazant@live.nl>
2020-12-08 21:07:23 +0100

    ADSpam Signage Complete
    +Sign is hidden in the first secret room of Level 2
    +Sign displays a bar that fills up when the player is wiggling (shaking his fleshlight)
    +The sign displays the letter A and D that intermittently appear, indicating the fact that the player has to spam AD and at the correct speed.

--- Assets/Scripts/Player/CharacterController2D.cs
+++ Assets/Scripts/Player/CharacterController2D.cs
@@ -106,8 +106,9 @@ public class CharacterController2D : MonoBehaviour
 			crouch = false;
 		}
 
-		//dashing left
-		if (Input.GetKeyDown(KeyCode.A))
+        #region DashCode
+        //dashing left
+        if (Input.GetKeyDown(KeyCode.A))
 		{
 			if ((doubleTapTime > Time.time && lastKeyCode == KeyCode.A) && isAbleToDash)
 			{
@@ -132,8 +133,9 @@ public class CharacterController2D : MonoBehaviour
 			}
 			lastKeyCode = KeyCode.D;
 		}
+        #endregion
 
-		playerAnim.SetBool("Grounded", false);
+        playerAnim.SetBool("Grounded", false);
 		Grounded = false;
 
 		// The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
