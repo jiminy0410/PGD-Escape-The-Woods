bd3cf1577fb952a1c8b5dec03456fa15f21269a4
Adriaan <adriaan.fazant@live.nl>
2022-05-11 16:25:44 +0200

    Base Analytics script

---
+++ Assets/Scripts/AnalyticsSystem.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Analytics;
+using UnityEngine.SceneManagement;
+
+public class AnalyticsSystem : MonoBehaviour
+{
+
+    public int deathCount;
+    public float timePlayed;
+
+
+    void Start()
+    {
+        AnalyticsEvent.GameStart();
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    public void SendAnalytics()
+    {
+        timePlayed = Time.time;
+
+        Dictionary<string, object> analyticsValues = new Dictionary<string, object>
+        {
+            {"TimePlayed", timePlayed},
+            {"DeathCount", deathCount}
+        };
+
+        AnalyticsEvent.Custom("Testing Metrics", analyticsValues);
+    }
+}
