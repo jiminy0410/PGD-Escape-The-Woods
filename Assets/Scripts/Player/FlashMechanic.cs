1c919d3d48aefb039a57df96433f54ae017d00ba
Adriaan Pronk <adriaan.fazant@live.nl>
2024-02-12 15:53:44 +0100

    Dunno, something I guess

--- Assets/Scripts/Player/FlashMechanic.cs
+++ Assets/Scripts/Player/FlashMechanic.cs
@@ -57,7 +57,7 @@ public class FlashMechanic : MonoBehaviour
             case Difficulty.Easy:
                 WiggleChargeMax = 0.7f;
                 chargeRateRecovery = 0.0005f;
-                lightDecay = 10f;
+                lightDecay = 7f;
                 defaultOuterRadius = 0f;
                 enlargedOuterRadius = 20;
                 standingChargeMin = 0.08f;
@@ -66,7 +66,7 @@ public class FlashMechanic : MonoBehaviour
             case Difficulty.Medium:
                 WiggleChargeMax = 0.5f;
                 chargeRateRecovery = 0.0005f;
-                lightDecay = 13f;
+                lightDecay = 10f;
                 defaultOuterRadius = 0f;
                 enlargedOuterRadius = 15;
                 standingChargeMin = 0.08f;
@@ -75,7 +75,7 @@ public class FlashMechanic : MonoBehaviour
             case Difficulty.Hard:
                 WiggleChargeMax = 0.5f;
                 chargeRateRecovery = 0.001f;
-                lightDecay = 16f;
+                lightDecay = 13f;
                 defaultOuterRadius = 0f;
                 enlargedOuterRadius = 15f;
                 standingChargeMin = 0.08f;
